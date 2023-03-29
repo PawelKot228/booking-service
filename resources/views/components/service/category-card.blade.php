314e5e840b10f55556ddd8779c2e6722d7f14649
kamu <74723463+PawelKot228@users.noreply.github.com>
2023-03-29 21:01:31 +0200

    add base images for service categories

--- resources/views/components/service/category-card.blade.php
+++ resources/views/components/service/category-card.blade.php
@@ -1,10 +1,10 @@
 
 
 <div class="hero"
-     style="background-image: url('https://media.istockphoto.com/id/1340519810/photo/angler-silhouette.jpg?b=1&s=170667a&w=0&k=20&c=tHI5igVbhfifVX6WGv9bdcV-C-cPgxrYNBykX54qb14=');"
+     style="background-image: url('{{ asset("content/services/$category->value.jpg") }}');"
 >
     <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content rounded-l">
+    <div class="hero-content text-center text-neutral-content rounded-l min-h-16 h-80">
         <div class="max-w-md">
             <h1 class="mb-5 text-3xl font-bold">{{ __($category->value) }}</h1>
             <div>
