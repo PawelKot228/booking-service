9b7552211b8c1d5f561c873fa4df5d32be0a9705
kamu <74723463+PawelKot228@users.noreply.github.com>
2023-04-02 19:21:03 +0200

    fix navigation when user is not logged

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-mark class="block h-9 w-auto"/>
                     </a>
                 </div>
@@ -15,11 +15,23 @@
                     <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                         {{ __('Home') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('services.categories') }}" :active="request()->routeIs('services.categories')">
+                    <x-nav-link href="{{ route('services.categories') }}"
+                                :active="request()->routeIs('services.categories')">
                         {{ __('Services') }}
                     </x-nav-link>
                 </div>
             </div>
+
+            @guest
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="ml-3 relative">
+                        <x-button-link :href="route('login')">
+                            {{ __('Log In') }}
+                        </x-button-link>
+                    </div>
+                </div>
+            @endguest
+
             @auth
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <div class="ml-3 relative">
@@ -27,7 +39,7 @@
                             <x-slot name="trigger">
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                     <button
-                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                         <img class="h-8 w-8 rounded-full object-cover"
                                              src="{{ Auth::user()->profile_photo_url }}"
                                              alt="{{ Auth::user()->name }}"/>
@@ -80,37 +92,48 @@
                     </div>
                 </div>
 
-                <!-- Hamburger -->
-                <div class="-mr-2 flex items-center sm:hidden">
-                    <button @click="open = ! open"
-                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M4 6h16M4 12h16M4 18h16"/>
-                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
-                                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M6 18L18 6M6 6l12 12"/>
-                        </svg>
-                    </button>
-                </div>
             @endauth
+            <div class="-mr-2 flex items-center sm:hidden">
+                <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M4 6h16M4 12h16M4 18h16"/>
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
+                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
+                </button>
+            </div>
 
         </div>
     </div>
 
-    @auth
 
-        <!-- Responsive Navigation Menu -->
-        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-            <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-responsive-nav-link>
-            </div>
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                {{ __('Home') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('services.categories') }}"
+                                   :active="request()->routeIs('services.categories')">
+                {{ __('Services') }}
+            </x-responsive-nav-link>
+        </div>
 
-            <!-- Responsive Settings Options -->
-            <div class="pt-4 pb-1 border-t border-gray-200">
+        <!-- Responsive Settings Options -->
+        <div class="pt-2 pb-1 border-t border-gray-200">
+            @guest
+                <div class="space-y-1">
+                    <x-responsive-nav-link href="{{ route('login') }}">
+                        {{ __('Log In') }}
+                    </x-responsive-nav-link>
+                </div>
+            @endguest
+
+            @auth
                 <div class="flex items-center px-4">
                     @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                         <div class="shrink-0 mr-3">
@@ -126,7 +149,6 @@
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <!-- Account Management -->
                     <x-responsive-nav-link href="{{ route('profile.show') }}"
                                            :active="request()->routeIs('profile.show')">
                         {{ __('Profile') }}
@@ -139,7 +161,6 @@
                         </x-responsive-nav-link>
                     @endif
 
-                    <!-- Authentication -->
                     <form method="POST" action="{{ route('logout') }}" x-data>
                         @csrf
 
@@ -149,7 +170,7 @@
                         </x-responsive-nav-link>
                     </form>
                 </div>
-            </div>
+            @endauth
         </div>
-    @endauth
+    </div>
 </nav>
