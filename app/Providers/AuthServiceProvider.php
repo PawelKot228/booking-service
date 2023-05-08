4983b9acaefa37f7a0c7e9540f3c2c3c1a14698e
pawel <74723463+PawelKot228@users.noreply.github.com>
2023-05-08 15:01:35 +0200

    Company Appointments, middleware

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Models\Appointment;
-use App\Models\Company;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
