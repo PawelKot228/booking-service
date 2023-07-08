0e7e6a6643a2ed4e1f0793cd4b978e1b093bbe37
kamu <74723463+PawelKot228@users.noreply.github.com>
2023-07-09 01:08:19 +0200

    seeders

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,35 +2,52 @@
 
 namespace Database\Seeders;
 
+use App\Enums\EmployeeRole;
 use App\Models\Company;
 use App\Models\Pivot\CompanyUser;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class CompanySeeder extends Seeder
 {
+    private Collection $users;
+
+    public function __construct()
+    {
+        $this->users = User::select('id')->get();
+    }
+
     public function run(): void
     {
-        $users = User::select('id')->get();
-        $syncUsers = [];
+        $this->createCompanies($this->users->find(1));
+        foreach ($this->users->random(200) as $user) {
+            $this->createCompanies($user);
+        }
+    }
 
-        foreach ($users->random(200) as $user) {
-            $companies = Company::factory()
-                ->count(random_int(1, 3))
-                ->create(['user_id' => $user->getKey()]);
+    private function createCompanies(User $user): void
+    {
+        $companies = Company::factory()
+            ->count(random_int(1, 3))
+            ->create(['user_id' => $user->getKey()]);
 
-            foreach ($companies as $company) {
-                foreach ($users->random(random_int(3, 6)) as $randomUser) {
-                    $syncUsers[] = [
-                        'company_id' => $company->getKey(),
-                        'user_id' => $randomUser->getKey()
-                    ];
-                }
+        foreach ($companies as $company) {
+            $syncUsers[] = [
+                'company_id' => $company->getKey(),
+                'user_id' => $user->getKey(),
+                'type' => EmployeeRole::OWNER,
+            ];
+
+            foreach ($this->users->random(random_int(3, 6)) as $randomUser) {
+                $syncUsers[] = [
+                    'company_id' => $company->getKey(),
+                    'user_id' => $randomUser->getKey(),
+                    'type' => EmployeeRole::EMPLOYEE,
+                ];
             }
         }
 
-        foreach (array_chunk($syncUsers, 500) as $userChunk) {
-            CompanyUser::insert($userChunk);
-        }
+        CompanyUser::insert($syncUsers);
     }
 }
