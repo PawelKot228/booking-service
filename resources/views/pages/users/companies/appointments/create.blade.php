c72a036e0e654dea3f11a8db187fc960131dbb5e
PawelKot228 <74723463+PawelKot228@users.noreply.github.com>
2025-02-18 16:39:38 +0100

    fixes

--- resources/views/pages/users/companies/appointments/create.blade.php
+++ resources/views/pages/users/companies/appointments/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $company->name }} - {{ __('Create category') }}
+            {{ $company->name }} - {{ __('Create appointment') }}
         </h2>
     </x-slot>
 
